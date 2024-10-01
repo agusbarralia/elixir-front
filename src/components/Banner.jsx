0a7ef3ca6a7d7573ab9d4fc466b2cb749e6929c5
LucasAlvarez <LucasAlvarezPersico@gmail.com>
2024-10-01 19:47:52 -0300

    Cambios en el banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,7 +11,7 @@ const Banner = () => {
   }, []);
 
   const handleScrollToContent = () => {
-    const contentSection = document.getElementById('content'); 
+    const contentSection = document.getElementById('content');
     if (contentSection) {
       contentSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
@@ -21,22 +21,27 @@ const Banner = () => {
     <div
       className="relative bg-cover bg-center h-screen"
       style={{
-        backgroundImage: "url('/banner_7_r.jpg')",
+        backgroundImage: "url('/banner_main.png')",
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat'
       }}
     >
-      <div className="absolute inset-0 bg-black bg-opacity-30 flex flex-col justify-center items-center">
-        <div className={`text-center text-white transition-opacity duration-700 ${fadeIn ? 'opacity-100' : 'opacity-0'}`}>
-          <h2 className="text-6xl font-bold animate-bounce">Elixir</h2>
-          <p className="mt-2 text-lg font-semibold animate-pulse">¡Uno se toma, el otro se saborea!</p>
-          <button 
-            onClick={handleScrollToContent} 
-            className="mt-4 px-6 py-3 bg-red-600 rounded hover:bg-red-700 transition duration-300 transform hover:scale-105"
-          >
-            Comprar Ahora
-          </button>
+      <div className="absolute inset-0 bg-black bg-opacity-30 flex flex-col justify-center items-right">
+        <div className={`text-right mr-20 p-20 text-white transition-opacity duration-700 ${fadeIn ? 'opacity-100' : 'opacity-0'}`}>
+          {/* Contenedor relativo para el logo y el botón */}
+          <div className="relative inline-block">
+            {/* Imagen del logo */}
+            <img src="/elixir_logo_eslogan.png" alt="Elixir Logo" className="w-128 animate-pulse" />
+            {/* Botón posicionado relativamente debajo del logo */}
+            <button 
+              onClick={handleScrollToContent} 
+              className="absolute left-1/2 transform -translate-x-1/2 mt-4 px-6 py-3 bg-red-600 rounded hover:bg-red-700 transition duration-300 transform hover:scale-105"
+              style={{ top: '100%' }}
+            >
+              Comprar Ahora
+            </button>
+          </div>
         </div>
       </div>
     </div>
